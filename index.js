7b1aa8fbd5393571f473eaa9b9dde271d648f7b6
Kajal kumari <109240333+kajal-jaiswal@users.noreply.github.com>
2023-10-19 22:44:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,53 +7,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-// app.set('view engine',(__dirname, '../ejs'));
 
-// let task = ["buy 1", " buy 2"];
-
-// mongoose.connect("mongodb://localhost:27017/todolistDB");
-
-// const ItemSchema = new mongoose.Schema({
-//     name : String
-// })
-
-// const Item = new mongoose.model('Items', ItemSchema);
-
-// const item1 = new Item({
-//     name : "Sharad"
-// })
-// const item2 = new Item({
-//     name : "Kajal"
-// })
-// const item3 = new Item({
-//     name : "Manish"
-// })
-// const defaultItems = [item1,item2,item3];
-
-// Item.insertMany(defaultItems)
-// .then(function(result){
-//     console.log("success");
-// })
-// .catch(function(error){
-//     console.log(error);
-// })
-
-// app.get("/",(req,res)=>{
-//     res.render("index.ejs",{
-//         task : Item,
-//     });
-// })
-// app.post("/",(req,res)=>{
-//     let newTask = req.body["newtask"];
-//     task.push(newTask);
-//     res.redirect("/");
-// })
-
-
-
-// app.listen(3000,()=>{
-//     console.log("server is running on port 3000");
-// })
 async function run(){
     await mongoose.connect("mongodb+srv://admin-kajal:1234kajal@cluster0.zhfyzum.mongodb.net/todolistDB").then(() => console.log("Database connected!"))
 .catch(err => console.log(err));
@@ -84,24 +38,7 @@ const item3 = new Item({
 //Storing items into an array
 const defaultItems = [item1, item2, item3];
  
-//In latest version of mongoose insertMany has stopped accepting callbacks
-//instead they use promises(Which Angela has not taught in this course)
-//So ".then" & "catch" are part of PROMISES IN JAVASCRIPT.
- 
-//PROMISES in brief(If something is wrong please correct me):
-//In JS, programmers encountered a problem called "callback hell", where syntax of callbacks were cumbersome & often lead to more problems.
-//So in effort to make it easy PROMISES were invented.
-//to learn more about promise visit : https://javascript.info/promise-basics
-//Or https://www.youtube.com/watch?v=novBIqZh4Bk
- 
-//! this to insert the data
-// Item.insertMany(defaultItems)
-//   .then(function(){
-//     console.log("Successfully saved into our DB.");
-//   })
-//   .catch(function(err){
-//     console.log(err);
-//   });
+
 
 
 const listSchema = new mongoose.Schema({
@@ -215,13 +152,7 @@ app.get("/:titleName", (req,res)=>{
 })
 
 
-// app.get("/work", function(req,res){
-//   res.render("list", {listTitle: "Work List", newListItems: workItems});
-// });
- 
-// app.get("/about", function(req, res){
-//   res.render("about");
-// });
+
  
 app.listen(3000, function() {
   console.log("Server started on port 3000");
